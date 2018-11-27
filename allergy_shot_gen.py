f75042796e5f4a8ac2ff86591a074db16ed49db2
jrheard <jrheard@cs.stanford.edu>
2018-11-27 14:46:49 -0800

    implement dry run

--- allergy_shot_gen.py
+++ allergy_shot_gen.py
@@ -1,10 +1,19 @@
 """This is a little program that generates taskwarrior tasks to help me keep track of
 when I need to go downtown for allergy shots.
 """
+import argparse
 import datetime
+
+# TODO give allergy tag high urgency
 
 
 # let's start by writing code to figure out the relevant days, and later on we can figure out taskwarrior stuff
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Generate a taskwarrior task that tells me when to get allergy shots.')
+    parser.add_argument('--dry-run', action='store_true')
+    return parser.parse_args()
+
 
 def weekday_to_string(weekday_int):
     """Takes a number between 0 and 6, returns a string like "Wednesday"."""
@@ -21,8 +30,14 @@ def weekday_to_string(weekday_int):
     ][weekday_int]
 
 
+def format_date(a_datetime):
+    return a_datetime.strftime('%Y-%m-%d')
+
 
 def main():
+    args = parse_args()
+
+    # TODO implement most_recent_shot_date arg
     today = datetime.datetime.today()
 
     # The allergy clinic is only open on weekdays,
@@ -45,8 +60,19 @@ def main():
     if days_to_subtract > 0:
         latest_possible = latest_possible - datetime.timedelta(days=days_to_subtract)
 
+    if args.dry_run:
+        print(
+            'task add +ALLERGY pri:H wait:{wait} due:{due} get allergy shots, earliest day is {weekday} {date}'.format(
+                wait=format_date(earliest_possible - datetime.timedelta(days=2)),
+                due=format_date(latest_possible),
+                weekday=weekday_to_string(earliest_possible.weekday()),
+                date=earliest_possible.strftime('%m/%d')
+            )
+        )
+    else:
+        # TODO
+        assert False
 
-    print('Latest possible is {}'.format(weekday_to_string(latest_possible.weekday())))
 
 
 if __name__ == '__main__':
